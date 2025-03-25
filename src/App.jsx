827cc3b5502007622accdb6a6520e8e4056c2907
MyUsername <rosanio.c@gmail.com>
2025-03-25 12:50:39 +0100

    Milestone 7 - Task Detail Page

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import { GlobalProvider } from "./context/GlobalContext";
 // COMPONENTS
 import TaskList from "./pages/TaskList";
 import AddTask from "./pages/AddTask";
-
+import TaskDetail from "./pages/TaskDetails";
 
 
 function App() {
@@ -27,6 +27,7 @@ function App() {
           <Routes>
             <Route path='/' element={<TaskList />} />
             <Route path='/add' element={<AddTask />} />
+            <Route path='/task/:id' element={<TaskDetail />} />
           </Routes>
 
         </BrowserRouter>
---
+++ src/pages/TaskDetails.jsx
@@ -0,0 +1,39 @@
+// UTILITY
+import { useParams } from "react-router-dom";
+import { useContext } from "react";
+
+
+// CONTEXT
+import { GlobalContext } from "../context/GlobalContext";
+
+
+export default function TaskDetail() {
+
+    const { id } = useParams();
+    const { tasks } = useContext(GlobalContext);
+
+    const task = tasks.find(task => task.id === parseInt(id));
+
+    if (!task) {
+        return <h2 className="debug">Task non trovata</h2>
+    }
+
+    const handleDelete = () => {
+        //logica    
+    }
+
+    return (
+        <>
+            <div>
+                <h1 className="debug">Dettagli task {task.id}</h1>
+
+                <p className="debug">Titolo: {task.title}</p>
+                <p className="debug">Descrizione: {task.description}</p>
+                <p className="debug">Status: {task.status}</p>
+                <p className="debug">Data di creazione: {new Date(task.createdAt).toLocaleDateString()}</p>
+
+                <button onClick={handleDelete}>Elimina Task</button>
+            </div>
+        </>
+    )
+}
